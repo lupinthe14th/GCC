4676c8306ea8c6cdef2ba3a4850fdbcbb2a8834b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-08 22:59:47 +0900

    Round A 2020 - Kick Start 2020/Plates: dp solution

---
+++ kickstart/Round2020/A/Plates/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "sample"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output:
+	// Case #1: 250
+	// Case #2: 180
+}
